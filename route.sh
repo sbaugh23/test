fdbc02cec3e78e2063e6f11567945d368568f66d
sbaugh23 <37468000+sbaugh23@users.noreply.github.com>
2026-01-22 04:04:43 +0100

    Add files via upload

---
+++ route.sh
@@ -0,0 +1,6 @@
+route delete 10.0.0.0/8
+route delete 192.168.0.0/16
+route delete 172.16.0.0/12
+route add 10.0.0.0/8 192.168.4.101
+route add 192.168.0.0/16 192.168.4.101
+route add 172.16.0.0/12 192.168.4.101
